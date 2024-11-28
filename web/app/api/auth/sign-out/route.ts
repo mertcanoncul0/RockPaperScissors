0c84906a7b82f53a7553eee3748c22cbae9cdfc9
Mertcan Öncül <mertcanoncul@Mertcan-MacBook-Air.local>
2024-11-28 22:34:06 +0300

    deleted files.

--- web/app/api/auth/sign-out/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function POST(request: NextRequest) {
-  const response = NextResponse.json({ message: 'Logged out' }, { status: 200 })
-  response.cookies.delete('token')
-  return response
-}
